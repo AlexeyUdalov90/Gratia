6bb1d50266205b9547f20c12c3c4deda75a0299e
AlexeyUdalov90 <armiman69@gmail.com>
2018-04-09 20:57:04 +0300

    settings of project

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var server = require('browser-sync');
+var minify = require('gulp-csso');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('style', function () {
+  gulp.src('sass/style.scss')
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(postcss([autoprefixer()]))
+      .pipe(gulp.dest('css'))
+      .pipe(minify())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('css'))
+      .pipe(server.stream())
+});
+
+gulp.task('images', function () {
+  return gulp.src('img/**/*.{png,jpg,svg}')
+      .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.svgo()
+      ]))
+      .pipe(gulp.dest('img'));
+});
+
+gulp.task('serve', ['style'], function () {
+  server.init({
+    server: '.',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch('sass/**/*.{scss,sass}', ['style']);
+  gulp.watch('*.html').on('change', server.reload);
+});
